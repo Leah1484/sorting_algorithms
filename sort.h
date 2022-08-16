41cbda158e9677a3dccd1b17a65c157bc5628b1a
Leah1484 <leahnjoki1484@gmail.com>
2022-08-16 12:46:16 -0700

    107-quick_sort_hoare.c

--- sort.h
+++ sort.h
@@ -1,27 +1,13 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#define UP 0
-#define DOWN 1
-
 /**
- * enum bool - enumeration of boolean value
- * @false: boolean value
- * @true: boolean value
- */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
-
-/**
- * struct listint_s - doubly linked list node
- * @prev: pointer to previous element of the list
- * @next: pointer to the next element of the list
- * @n: integer value
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -30,14 +16,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void swap_ints(int *a, int *b);
-
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-void bubble_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
